c448586334f582ac1f4f25c282447156ca88888e
Maher-Wali <walimaher00@gmail.com>
2024-04-10 07:04:03 +0100

    linking fixes

--- index.php
+++ index.php
@@ -87,7 +87,7 @@
     <section class="contact-form-section" id="contact-form-section">
         <div class="container-fluid mt-5" style="background-color: #26272b; color: #fff; padding: 20px;">
             <h1>Contact Us</h1>
-            <form action="../contact_us/send_email.php" method="post">
+            <form action="send_email.php" method="post">
                 <div class="mb-3">
                     <label for="email" class="form-label">Email:</label>
                     <input type="email" class="form-control" id="email" name="email" style="background-color: #33353d; color: #eceeef;" required>
